07ca9e8a6d4350f0e591c04277dc4a7118395f32
arshan19 <aliarshan1934@gmail.com>
2024-02-09 12:05:09 +0530

    Added multer and cloudinary files for file upload

--- src/index.js
+++ src/index.js
@@ -2,26 +2,19 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
-dotenv.config({
-    path: './env'
-})
+import {app} from './app.js'
+dotenv.config({ path: "../.env"});
 
 
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT || 8000, ()=>{
-        console.log(` Server is listening on Port : ${process.env.PORT}...`);
+        console.log(`⚙️ Server is Running on Port : ${process.env.PORT}...`);
     })
 })
 .catch((err)=>{
     console.log("MongoDB connection error: " , err);
 })
-
-
-
-
-
-
 
 
 
