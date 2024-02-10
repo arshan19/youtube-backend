3c6461bb268210d628f7af68488addb9633c152d
arshan19 <aliarshan1934@gmail.com>
2024-02-11 02:05:32 +0530

    remove some error in package.json file , and in index file

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ dotenv.config({ path: "../.env"});
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT || 8000, ()=>{
-        console.log(`⚙️ Server is Running on Port : ${process.env.PORT}...`);
+        console.log(`⚙️ Server is Running on Port : ${process.env.PORT}`);
     })
 })
 .catch((err)=>{
