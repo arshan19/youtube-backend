2ad40fc78208dbda7a2216df705fb6e3b405286c
arshan19 <aliarshan1934@gmail.com>
2024-02-20 14:41:56 +0530

    added all  userRoutes as well

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,20 @@
 //here we implement middleware just before method of post or anything going to run
 
 import { Router } from "express";
-import { logOutUser, loginUser, registerUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
+import { 
+            logOutUser,
+            loginUser, 
+            registerUser,
+            refreshAccessToken, 
+            changeCurrentPassword, 
+            getCurrentUser, 
+            updateAccountDetails, 
+            updateUserAvatar, 
+            updateUserCoverImage, 
+            getUserChannelProfile, 
+            getWatchHistory 
+        } from "../controllers/user.controller.js";
+        
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
